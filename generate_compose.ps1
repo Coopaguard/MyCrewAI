9d8a1a1e596d1a24f67784353e1c6ccbff3991b7
Coopa <kill_azf@hotmail.fr>
2025-03-25 21:58:47 +0100

    adding OpenWebUI configured

--- generate_compose.ps1
+++ generate_compose.ps1
@@ -7,9 +7,9 @@ $GPU_INSTANCES = $configJson.GPU_INSTANCES;
 $GPUS_COUNT = $configJson.GPUS_COUNT;
 $SHARED_PATH_HOST = $configJson.SHARED_PATH_HOST;
 $CUSTOM_API_KEY = $configJson.CUSTOM_API_KEY;
-$POSTGRES_USER = $configJson.POSTGRES_USER;
-$POSTGRES_PASSWORD = $configJson.POSTGRES_PASSWORD;
-$POSTGRES_DB = $configJson.POSTGRES_DB;
+$POSTGRES_USER = $configJson.CREWAI_POSTGRES_USER;
+$POSTGRES_PASSWORD = $configJson.CREWAI_POSTGRES_PASSWORD;
+$POSTGRES_DB = $configJson.CREWAI_POSTGRES_DB;
 $RESTART = $configJson.RESTART_OPTION;
 $WEBUI_DOCKER_TAG = "main";
 if ($configJson.WEBUI_DOCKER_TAG -ne "") {
@@ -19,6 +19,9 @@ $WEBUI_PORT = "3000";
 if ($configJson.OPEN_WEBUI_PORT -ne "") {
   $WEBUI_PORT = $configJson.OPEN_WEBUI_PORT;
 }
+
+$CPULIMIT = $configJson.USABLE_CPU_CORES_COUNT;
+$MEMLIMIT = $configJson.MEMORY_LIMIT;
 
 # Vérifier si l'image Docker 'crewai-studio' existe localement
 $imageExists = docker images -q crewai-studio
@@ -39,11 +42,17 @@ if (-not $imageExists) {
     Write-Output "L'image 'crewai-studio' existe déjà localement."
 }
 
+# set config params
+Write-Host "Voulez-vous ajouter une interaface pour acceder l'IA ? (y/n*)";
+$confirmationUI = [System.Console]::ReadKey().Key.ToString();
+
 # Initialiser le contenu du fichier docker-compose.yaml
 $composeContent = @"
 services:
   ollama:
     image: ollama/ollama:latest
+    mem_limit: ${MEMLIMIT}
+    cpu_count: ${CPULIMIT}
     restart: ${RESTART}
     ports:
       - "11434:11434"
@@ -69,6 +78,7 @@ if ($GPUS_COUNT -gt 0) {
 "@
 }
 
+#PG Sql
 $composeContent += @"
 
   postgres-db:
@@ -81,7 +91,23 @@ $composeContent += @"
     ports:
       - "5432:5432"
     volumes:
-      - postgres-data:/var/lib/postgresql/data   
+      - postgres-data:/var/lib/postgresql/data
+"@ 
+if ($confirmationUI -eq 'y') {
+  $composeContent += @"
+
+      - ./init-database.sql:/docker-entrypoint-initdb.d/init-database.sql
+
+"@
+} else {
+  $composeContent += @"
+
+
+"@
+}
+
+#CREW AI
+$composeContent += @"
   crewai-studio:
     image: crewai-studio
     restart: ${RESTART}
@@ -99,28 +125,25 @@ $composeContent += @"
 
 "@
 
-# 
-Write-Host "Voulez-vous ajouter une interaface pour acceder l'IA ? (y/n*)";
-$confirmationUI = [System.Console]::ReadKey().Key.ToString();
+# Add Open Web UI
 if ($confirmationUI -eq 'y') {
     $composeContent += @"
   open-webui:
-    build:
-      context: .
-      args:
-        OLLAMA_BASE_URL: '/ollama'
-      dockerfile: Dockerfile
     image: ghcr.io/open-webui/open-webui:${WEBUI_DOCKER_TAG}
-    container_name: open-webui
+    environment:
+      - OLLAMA_API_BASE_URL=http://ollama:11434
+      - OLLAMA_API_URL=http://ollama:11434
     volumes:
       - open-webui:/app/backend/data
     depends_on:
       - ollama
+      - postgres-db
     ports:
       - ${WEBUI_PORT}:8080
     environment:
-      - 'OLLAMA_BASE_URL=http://ollama:11434'
-      - 'WEBUI_SECRET_KEY='
+      - OLLAMA_BASE_URL=http://ollama:11434
+      - WEBUI_SECRET_KEY=
+      - DATABASE_URL=postgresql://openwebui:password@1Panel-postgresql-DMNz:5432/openwebui
     extra_hosts:
       - host.docker.internal:host-gateway
     restart: ${RESTART}
@@ -145,6 +168,20 @@ if ($confirmationUI -eq 'y') {
 # Écrire le contenu dans le fichier docker-compose.yaml
 $composeContent | Out-File -FilePath docker-compose.yaml -Encoding utf8
 
+# création du fichier de configuration supplémentaire
+if ($confirmationUI -eq 'y') {
+  $pguser = $configJson.OPENWEB_POSTGRES_USER;
+  $pgpwd = $configJson.OPENWEB_POSTGRES_PASSWORD;
+  $pgdb = $configJson.OPENWEB_POSTGRES_db;
+
+  $composeSqlPG = @"
+CREATE USER $pguser WITH PASSWORD '$pgpwd';
+CREATE DATABASE $pgdb;
+GRANT ALL PRIVILEGES ON DATABASE $pgdb TO $pguser;
+"@
+
+$composeSqlPG | Out-File -FilePath init-database.sql -Encoding utf8
+}
 
 Write-Output "Fichier docker-compose.yaml genere avec succes !"
 
